d1a95dc30221cfe09947d2a28a24be1777017ea7
Luiz-Otavio "Z" Zorzella <zorzella@gmail.com>
2019-11-29 23:00:17 -0800

    Reformat

--- src/ZrFuncTypeDescription.cpp
+++ src/ZrFuncTypeDescription.cpp
@@ -28,9 +28,7 @@ const std::string ZrFuncTypeDescription::humanName() const {
 
 const double ZrFuncTypeDescription::humanNotch() const { return m_humanNotch; }
 
-const bool ZrFuncTypeDescription::isOnOff() const {
-  return m_isOnOff;
-}
+const bool ZrFuncTypeDescription::isOnOff() const { return m_isOnOff; }
 
 const double ZrFuncTypeDescription::humanToOscValue(double human) const {
   switch (m_type) {
@@ -56,7 +54,8 @@ const double ZrFuncTypeDescription::oscValueToHuman(double oscValue) const {
   }
 }
 
-const double ZrFuncTypeDescription::oscValueToRoundedHuman(double oscValue) const {
+const double ZrFuncTypeDescription::oscValueToRoundedHuman(
+    double oscValue) const {
   switch (m_type) {
     case GAIN:
       return ZrGain::oscValueToRoundedHuman(oscValue);
